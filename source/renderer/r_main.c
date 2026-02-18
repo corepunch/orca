336669d483bb108f58e5301f88388fc28abf6045
Igor Chernakov <igor.chernakov@gmail.com>
2026-02-18 20:42:12 +0100

    Update r_main.c

--- source/renderer/r_main.c
+++ source/renderer/r_main.c
@@ -277,68 +277,64 @@ R_DrawEntity(struct ViewDef const* view, struct ViewEntity* ent)
   
   // Handle mesh pointer boxing: mesh can be either a real pointer or a boxed tag value
   struct model /*const*/* model = NULL;
-  if (ent->mesh) {
-    if (BOX_IS_PTR((uintptr_t)ent->mesh)) {
-      // Real mesh pointer - extract the model
-      model = ((struct Mesh const*)BOX_GET_PTR((uintptr_t)ent->mesh))->model;
+  struct vec4 zero = {0};
+  if (!ent->mesh) {
+    // Default case when mesh is NULL - treat as rectangle
+    if (memcmp(&ent->borderWidth, &zero, sizeof(struct vec4))) {
+      model = tr.models[MD_ROUNDED_BORDER];
+    } else {
+      model = tr.models[MD_ROUNDED_RECT];
     }
-    // else: mesh is a boxed tag value, will be handled below
+  } else if (BOX_IS_PTR((uintptr_t)ent->mesh)) {
+    // Real mesh pointer - extract the model
+    model = ((struct Mesh const*)BOX_GET_PTR((uintptr_t)ent->mesh))->model;
+  } else switch (((uintptr_t)ent->mesh & MESH_TAG_MASK)) {
+    case BOXED_MESH_CINEMATIC:
+      _UpdateCinematicEntity(ent);
+      shader = &tr.shaders[SHADER_CINEMATIC];
+      model = tr.models[MD_RECTANGLE];
+      break;
+    case BOXED_MESH_NINEPATCH:
+      model = CreateNinePatchMesh(ent);
+      break;
+    case BOXED_MESH_RECTANGLE:
+      if (memcmp(&ent->borderWidth, &zero, sizeof(struct vec4))) {
+        model = tr.models[MD_ROUNDED_BORDER];
+      } else {
+        model = tr.models[MD_ROUNDED_RECT];
+      }
+      break;
+    case BOXED_MESH_PLANE:
+      model = tr.models[MD_PLANE];
+      break;
+    case BOXED_MESH_DOT:
+      model = tr.models[MD_DOT];
+      break;
+    case BOXED_MESH_CAPSULE:
+      model = tr.models[MD_CAPSULE];
+      shader = &tr.shaders[SHADER_BUTTON];
+      break;
+    case BOXED_MESH_ROUNDED_BOX:
+      model = tr.models[MD_ROUNDED_BOX];
+      shader = &tr.shaders[SHADER_ROUNDEDBOX];
+      break;
+    case BOXED_MESH_TEAPOT:
+      // Teapot could use a sphere or custom model in the future
+      model = tr.models[MD_PLANE]; // placeholder
+      break;
   }
   
   if (ent->flags & RF_DRAW_DEBUG) {
     _DrawDebug(ent, view);
     return NOERROR;
   }
-  struct vec4 zero = {0};
   
   // If no model yet, check for boxed mesh tags
-  if (model == NULL) {
-    if (ent->mesh && !BOX_IS_PTR((uintptr_t)ent->mesh)) {
-      enum boxed_mesh_type mesh_tag = (enum boxed_mesh_type)((uintptr_t)ent->mesh & MESH_TAG_MASK);
-      
-      switch (mesh_tag) {
-        case BOXED_MESH_CINEMATIC:
-          _UpdateCinematicEntity(ent);
-          shader = &tr.shaders[SHADER_CINEMATIC];
-          model = tr.models[MD_RECTANGLE];
-          break;
-        case BOXED_MESH_NINEPATCH:
-          model = CreateNinePatchMesh(ent);
-          break;
-        case BOXED_MESH_RECTANGLE:
-          if (memcmp(&ent->borderWidth, &zero, sizeof(struct vec4))) {
-            model = tr.models[MD_ROUNDED_BORDER];
-          } else {
-            model = tr.models[MD_ROUNDED_RECT];
-          }
-          break;
-        case BOXED_MESH_PLANE:
-          model = tr.models[MD_PLANE];
-          break;
-        case BOXED_MESH_DOT:
-          model = tr.models[MD_DOT];
-          break;
-        case BOXED_MESH_CAPSULE:
-          model = tr.models[MD_CAPSULE];
-          shader = &tr.shaders[SHADER_BUTTON];
-          break;
-        case BOXED_MESH_ROUNDED_BOX:
-          model = tr.models[MD_ROUNDED_BOX];
-          shader = &tr.shaders[SHADER_ROUNDEDBOX];
-          break;
-        case BOXED_MESH_TEAPOT:
-          // Teapot could use a sphere or custom model in the future
-          model = tr.models[MD_PLANE]; // placeholder
-          break;
-      }
-    } else if (!ent->mesh) {
-      // Default case when mesh is NULL - treat as rectangle
-      if (memcmp(&ent->borderWidth, &zero, sizeof(struct vec4))) {
-        model = tr.models[MD_ROUNDED_BORDER];
-      } else {
-        model = tr.models[MD_ROUNDED_RECT];
-      }
-    }
+  if (model) {
+    // all good, skip
+  } else if (ent->mesh && !BOX_IS_PTR((uintptr_t)ent->mesh)) {
+
+  } else if (!ent->mesh) {
   }
   
   struct shader_universal_target const *target = &shader->shader->target;
