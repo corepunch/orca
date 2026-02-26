cc4a8a80edbada0124a08a92b7eeb658ef45b48f
Igor Chernakov <igor.chernakov@gmail.com>
2026-02-26 13:50:11 +0100

    Remove commented-out legacy StackView code
    Remove large blocks of commented-out/legacy helper functions and an old MeasureOverride implementation from source/UIKit/StackView.c. Also clean up a small commented branch in _GetContentSize and remove an unused local (primary) in ArrangeOverride. This cleans up dead code and improves readability and maintainability of the StackView implementation.

--- source/UIKit/StackView.c
+++ source/UIKit/StackView.c
@@ -4,34 +4,6 @@
 
 #include <source/UIKit/UIKit.h>
 
-//float
-//Node2D_Align(Node2DPtr, float min, float max, enum Direction, int align);
-
-//static void
-//_ReverseArrangeInAxis(StackViewPtr pStackView,
-//                      lpObject_t child,
-//                      enum Direction axis,
-//                      float* counter)
-//{
-//  Node2DPtr subview = GetNode2D(child);
-//
-//  if (!subview)
-//    return;
-//
-//  _ReverseArrangeInAxis(pStackView, OBJ_GetNext(subview->_object), axis, counter);
-//
-//  struct transform2* t = &subview->LayoutTransform;
-//  PROP_Update(Node2D_GetProperty(subview->_object, kNode2DLayoutTransform));
-//  Node2D_SetFrame(subview, kBox3FieldX + axis, ((float*)t)[axis] + *counter);
-//
-//  if (Node2D_IsFrameSet(subview, kBox3FieldWidth + axis)) {
-//    float frame = Node2D_GetFrame(subview, kBox3FieldWidth + axis);
-//    float spacing = pStackView->Spacing;
-//    float margin = TOTAL_MARGIN(subview, axis);
-//    *counter += frame + margin + spacing;
-//  }
-//}
-
 static float
 _GetContentSize(lpObject_t hObject,
                 StackViewPtr pStackView,
@@ -43,41 +15,10 @@ _GetContentSize(lpObject_t hObject,
     Node2DPtr subview = GetNode2D(child);
     if (Node2D_IsFrameSet(subview, kBox3FieldWidth + axis)) {
       value += Node2D_GetFrame(subview, kBox3FieldWidth + axis) + pStackView->Spacing;
-      //        } else {
-      //            return 0;
     }
   }
   return value > 0 ? value - pStackView->Spacing : value;
 }
-
-// static enum ui_align
-//_GetContentAlignment(lpObject_t  hObject, enum Direction axis)
-//{
-//	lpObject_t  child = OBJ_GetFirstChild(hObject);
-//	return child ? NODE2D_FRAME(GetNode2D(child), Alignment, axis) :
-// kUIAlignLeft;
-// }
-
-//static void
-//_SetPosition(Node2DPtr subview, enum Direction axis, float counter)
-//{
-//  struct transform2* t = &subview->LayoutTransform;
-//  PROP_Update(Node2D_GetProperty(subview->_object, kNode2DLayoutTransform));
-//  float translation = ((float*)t)[axis] + counter + MARGIN_TOP(subview, axis);
-//  Node2D_SetFrame(subview, kBox3FieldX + axis, translation);
-//}
-//
-//static float
-//_GetNodeSize(Node2DPtr subview, enum Direction axis, float spacing)
-//{
-//  if (Node2D_IsFrameSet(subview, kBox3FieldWidth + axis)) {
-//    float frame = Node2D_GetFrame(subview, kBox3FieldWidth + axis);
-//    float margin = TOTAL_MARGIN(subview, axis);
-//    return frame + margin + spacing;
-//  } else {
-//    return 0;
-//  }
-//}
 
 static enum NodeProperties alignment[] = {
   kNodeHorizontalAlignment,
@@ -162,80 +103,8 @@ _Arrange(lpObject_t hObject,
       counter += rect.width + pStackView->Spacing + distributed;
       OBJ_SendMessageW(child, kEventArrange, 0, &rect);
     }
-
-//    lpProperty_t p = Node_GetProperty(subview->_object, alignment[opposite]);
-//    int* chd = &NODE2D_FRAME(subview, Alignment, opposite);
-//    uint32_t align = p ? *chd : pStackView->AlignItems;
-//    float x = Node2D_Align(subview,
-//                           oppositeBounds.min,
-//                           oppositeBounds.max,
-//                           opposite,
-//                           align);
-//    
-//    Node2D_SetFrame(subview, kBox3FieldX + opposite, x);
   }
 }
-//
-//static bool_t
-//_WillStretch(Node2DPtr node, StackViewCPtr pStackView)
-//{
-//  return !NODE2D_FRAME(node, Alignment, pStackView->Direction);
-//}
-//
-//HANDLER(StackView, MeasureOverride)
-//{
-//  Node2DPtr pNode2D = GetNode2D(hObject);
-//  Size_t data = {
-//    pMeasureOverride->width - TOTAL_PADDING(pNode2D, 0),
-//    pMeasureOverride->height - TOTAL_PADDING(pNode2D, 1),
-//  };
-//  
-//  float *datasize = (float*)&data;
-//  float size[2] = {0}, fsize[2] = {0};
-//  uint32_t dwNumAutos = 0;
-//  uint32_t const primary = pStackView->Direction;
-//  float const gap = pStackView->Spacing;
-//
-//  size[primary] = -gap;
-//
-//  FOR_EACH_LAYOUTABLE(hChild, pNode2D->_object)
-//  {
-//    if (_WillStretch(GetNode2D(hChild), pStackView)) {
-//      dwNumAutos++;
-//      continue;
-//    }
-//    uint32_t s = OBJ_SendMessageW(hChild, kEventMeasure, 0, &data);
-//    fsize[0] = LOWORD(s);
-//    fsize[1] = HIWORD(s);
-//    datasize[primary] -= fsize[primary] + gap;
-//    size[primary] += fsize[primary] + gap;
-//    // workaround for Kanzi stacks
-//#ifdef KANZI_SUPPORT
-//    if (primary == 1 || fsize[!primary] != data.Size[!primary] ||
-//        OBJ_GetFirstChild(hChild))
-//#endif
-//      size[!primary] = MAX(fsize[!primary], size[!primary]);
-//  }
-//
-//  if (dwNumAutos > 0)
-//  {
-//    float spacing = gap * (dwNumAutos - 1);
-//    datasize[primary] = (datasize[primary] - spacing) / dwNumAutos;
-//    FOR_EACH_LAYOUTABLE(hChild, pNode2D->_object)
-//    {
-//      if (!_WillStretch(GetNode2D(hChild), pStackView))
-//        continue;
-//      uint32_t s = OBJ_SendMessageW(hChild, kEventMeasure, 0, &data);
-//      fsize[0] = LOWORD(s);
-//      fsize[1] = HIWORD(s);
-//      size[!primary] = MAX(fsize[!primary], size[!primary]);
-//    }
-//    size[primary] = (&pMeasureOverride->width)[primary] - TOTAL_PADDING(pNode2D, primary);
-//  }
-//  
-//  return MAKEDWORD((MAX(0, size[0]) ? MAX(0, size[0]) : datasize[0]) + TOTAL_PADDING(pNode2D, 0),
-//                   (MAX(0, size[1]) ? MAX(0, size[1]) : datasize[1]) + TOTAL_PADDING(pNode2D, 1));
-//}
 
 HANDLER(StackView, MeasureOverride)
 {
@@ -273,7 +142,6 @@ HANDLER(StackView, ArrangeOverride)
 {
   Node2DPtr pNode2D = GetNode2D(hObject);
   rect_t r = *pArrangeOverride;
-//  uint32_t const primary = pStackView->Direction;
 
   switch (pStackView->Direction) {
     case kDirectionHorizontal:
