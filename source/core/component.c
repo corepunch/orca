a4b251ff8d5bad84dc793661a86b64b5acbc3d5f
Igor Chernakov <igor.chernakov@gmail.com>
2026-03-22 18:00:20 +0100

    Forced crash for logging

--- source/core/component.c
+++ source/core/component.c
@@ -16,10 +16,10 @@ struct component*
 OBJ_AddComponent(lpObject_t pobj, uint32_t class_id)
 {
   lpcClassDesc_t cls = OBJ_FindClassW(class_id);
-  if (!cls) {
-    Con_Error("class_id 0x%08x not found\n", class_id);
-    return NULL;
-  }
+//  if (!cls) {
+//    Con_Error("class_id 0x%08x not found\n", class_id);
+//    return NULL;
+//  }
   uint32_t clsSize = sizeof(struct component) + cls->ClassSize;
   uint32_t propsSize = cls->NumProperties * sizeof(void*);
   struct component* comp = ZeroAlloc(clsSize + propsSize);
