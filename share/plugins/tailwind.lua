90899edaca02fa00e0678943a9a39aa0e23144cf
Igor Chernakov <igor.chernakov@gmail.com>
2026-03-24 20:22:33 +0100

    Update tailwind.lua

--- share/plugins/tailwind.lua
+++ share/plugins/tailwind.lua
@@ -1,4 +1,4 @@
-local has_config, config = pcall(require, "config.tailwind")
+local has_config = false -- , config = pcall(require, "config.tailwind")
 local extends = { "colors" }
 local spacing = { 0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 16, 20, 24, 32, 40, 48, 56, 64 }
 local style = {}
